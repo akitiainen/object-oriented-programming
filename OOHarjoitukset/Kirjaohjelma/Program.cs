67a911edf66e7ea0725027763365b074cc9f7431
akitiainen <aki.tiainen@hotmail.fi>
2019-01-08 15:22:12 +0200

    Kirjaohjelma
    initial commit

---
+++ OOHarjoitukset/Kirjaohjelma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kirjaohjelma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
