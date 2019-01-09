a127f830a3c382f66a49fee4728a2e706940fa80
akitiainen <aki.tiainen@hotmail.fi>
2019-01-09 10:35:59 +0200

    CarTask
    initial commit

---
+++ OOHarjoitukset/CarTask/Car.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarTask
+{
+    class Car
+    {
+    }
+}
---
+++ OOHarjoitukset/CarTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
