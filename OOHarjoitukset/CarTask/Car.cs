4504a87de7b65f0697140e78c71ff7bf07dd2720
akitiainen <aki.tiainen@hotmail.fi>
2019-01-09 11:26:00 +0200

    CarTask v0.9

--- OOHarjoitukset/CarTask/Car.cs
+++ OOHarjoitukset/CarTask/Car.cs
@@ -6,5 +6,31 @@ namespace CarTask
 {
     class Car
     {
+        public string brand;
+        public int speed;
+
+        public Car()
+        {
+            this.brand = null;
+            this.speed = 0;
+        }
+
+        public void AskData()
+        {
+            Console.WriteLine("Syötä auton merkki: ");
+            this.brand = Console.ReadLine();
+            Console.WriteLine("Kuis kovaa auto kulkee???");
+            while(!int.TryParse(Console.ReadLine(), out this.speed))
+            {
+                Console.Clear();
+                Console.WriteLine("yritäppä uusiks ja laita kuule numeroita");
+
+            }
+        }
+
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"auton merkki: {this.brand}\nauton nopeus: {this.speed}");
+        }
     }
 }
--- OOHarjoitukset/CarTask/Program.cs
+++ OOHarjoitukset/CarTask/Program.cs
@@ -6,7 +6,11 @@ namespace CarTask
     {
         static void Main(string[] args)
         {
-            
+            Car auto = new Car();
+            auto.AskData();
+            Console.ReadKey();
+            auto.ShowCarInfo();
+            Console.ReadKey();
         }
     }
 }
