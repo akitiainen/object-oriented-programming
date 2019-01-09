ef62de87f4627e947e0c1aae599028bc9d5af28b
akitiainen <aki.tiainen@hotmail.fi>
2019-01-09 10:13:59 +0200

    Kirjaohjelma
    initial commit

--- OOHarjoitukset/Kirjaohjelma/Book.cs
+++ OOHarjoitukset/Kirjaohjelma/Book.cs
@@ -1,10 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Globalization;
 
 namespace Kirjaohjelma
 {
     class Book
     {
+        public string title;
+        public string author;
+        public string id;
+        public decimal price;
+
+        public Book(string title, string author, string id, decimal price)
+        {
+            this.title = title;   
+            this.author = author;
+            this.id = id;
+            this.price = price;
+        }
+
+        public string PrintInfo()
+        {
+            return  $"Kirjan nimi: {this.title}\n" +
+                    $"Kirjoittaja: {this.author}\n" +
+                    $"Kirjan id-numero: {this.id}\n" +
+                    $"Kirjan hinta: {this.price.ToString("C", CultureInfo.CurrentCulture)}\n";
+        }
+
+        public string CompareBooks(Book book)
+        {
+            string text = string.Empty;
+
+            if (this.price > book.price)
+                text = $"{this.title} on kalliimpi kuin {book.title}";
+            else if (this.price == book.price)
+                text = $"Kirjat {this.title} ja {book.title} ovat saman hintaisia.";
+            else
+                text = $"{book.title} on kalliimpi kuin {this.title}";
+            return text;
+        }
+        
+
+        
     }
 }
--- OOHarjoitukset/Kirjaohjelma/Program.cs
+++ OOHarjoitukset/Kirjaohjelma/Program.cs
@@ -6,7 +6,22 @@ namespace Kirjaohjelma
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Kirjaohjelma\n");
+            
+            Book newBook = new Book("Aapinen",
+                                    "Mikael Agricola",
+                                    "00001",
+                                    17.50M);
+            Console.WriteLine(newBook.PrintInfo());
+            Console.ReadKey();
+
+            Book newBook2 = new Book("lol xd", "iids", "10500", 1000500.12M);
+            Console.WriteLine(newBook2.PrintInfo());
+            Console.ReadKey();
+
+            Console.WriteLine( newBook.CompareBooks(newBook2));
+            Console.ReadKey();
         }
     }
 }
