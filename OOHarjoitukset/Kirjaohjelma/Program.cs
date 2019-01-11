fd72d1f0dc4734e2f3f2d655a2e47ec58e9e2086
akitiainen <aki.tiainen@hotmail.fi>
2019-01-11 13:25:03 +0200

    Kirjaohjelma v1.1

--- OOHarjoitukset/Kirjaohjelma/Program.cs
+++ OOHarjoitukset/Kirjaohjelma/Program.cs
@@ -7,21 +7,37 @@ namespace Kirjaohjelma
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.WriteLine("Kirjaohjelma\n");
-            
+            Intro();
             Book newBook = new Book("Aapinen",
                                     "Mikael Agricola",
                                     "00001",
                                     17.50M);
-            Console.WriteLine(newBook.PrintInfo());
-            Console.ReadKey();
-
             Book newBook2 = new Book("lol xd", "iids", "10500", 1000500.12M);
-            Console.WriteLine(newBook2.PrintInfo());
-            Console.ReadKey();
+            var input = Console.ReadKey();
+            switch (input.Key)
+            {
+                
+                case ConsoleKey.D1:
+                    Console.Clear();
+                    Console.WriteLine(newBook.PrintInfo());
+                    Console.WriteLine(newBook2.PrintInfo());
+                    break;
 
-            Console.WriteLine(newBook.CompareBooks(newBook2));
+                case ConsoleKey.D2:
+                    Console.Clear();
+                    Console.WriteLine(newBook.CompareBooks(newBook2));
+                    break;
+            }
+
+            
             Console.ReadKey();
+        }
+
+        static void Intro()
+        {
+            Console.WriteLine("Tämä on kirjaohjelma. Valitse aliohjelma painamalla numeroa\n" +
+                                "1. Tulostaa tallennetut kirjat\n" +
+                                "2. Vertailee kirjojen hintoja");
         }
     }
 }
