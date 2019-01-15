3cc7910d37755e2ddee40f68ae786d9ab3bfd38e
akitiainen <aki.tiainen@hotmail.fi>
2019-01-15 08:50:50 +0200

    CompanyProgram
    initial commit

--- OOHarjoitukset/CompanyProgram/Company.cs
+++ OOHarjoitukset/CompanyProgram/Company.cs
@@ -6,5 +6,19 @@ namespace CompanyProgram
 {
     class Company
     {
+        public string title;
+        public string address;
+        public string phone;
+        public decimal outcome;
+        public decimal expence;
+
+        public Company()
+        {
+        }
+
+        public Company(string t, string a, string p, decimal o, decimal e)
+        {
+
+        }
     }
 }
--- OOHarjoitukset/CompanyProgram/Program.cs
+++ OOHarjoitukset/CompanyProgram/Program.cs
@@ -6,7 +6,7 @@ namespace CompanyProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
