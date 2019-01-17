6b66477fa96924b13193152a71c177be7438d71e
akitiainen <aki.tiainen@hotmail.fi>
2019-01-17 13:51:15 +0200

    CompanyProgram v1.0

--- OOHarjoitukset/CompanyProgram/Company.cs
+++ OOHarjoitukset/CompanyProgram/Company.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Globalization;
 
 namespace CompanyProgram
 {
@@ -9,16 +10,53 @@ namespace CompanyProgram
         public string title;
         public string address;
         public string phone;
-        public decimal outcome;
-        public decimal expence;
+        public decimal income;
+        public decimal expense;
 
         public Company()
         {
         }
 
-        public Company(string t, string a, string p, decimal o, decimal e)
+        public Company(string t, string a, string p, decimal i, decimal o)
         {
+            title = t;
+            address = a;
+            phone = p;
+            income = i;
+            expense = o;
+        }
 
+        public Company (Company previousCompany)
+        {
+            title = previousCompany.title;
+            address = previousCompany.address;
+            phone = previousCompany.phone;
+            income = previousCompany.income;
+            expense = previousCompany.expense;
+        }
+
+        public string Profit()
+        {
+            string s = string.Empty;
+            decimal profit = (income - expense) / expense;
+
+            if (profit < 1)
+                s = "tämä ei ole hyväksyttävää";
+            else if (profit < 2)
+                s = "on kyl huono tulos";
+            else if (profit < 3)
+                s = "ihan ok tulos";
+            else
+                s = "on kyl hyvä tulos";
+
+            return $"Voittoprosentti {profit.ToString("P")}: {s}";
+        }
+
+        public override string ToString()
+        {
+            return "Firma:\t" + title + "\nOsoite:\t" + address
+                + "\npuh.\t" + phone + "\nTulot:\t" + income.ToString("c", CultureInfo.CurrentCulture) +
+                "\nMenot:\t" + expense.ToString("c", CultureInfo.CurrentCulture);
         }
     }
 }
--- OOHarjoitukset/CompanyProgram/Program.cs
+++ OOHarjoitukset/CompanyProgram/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
+
 namespace CompanyProgram
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("TÄS OIS PARI VIRMAA\n" +
+                "______________________\n");
+            Company firm = new Company("iidan muansiirtovirma",
+                                       "Sorakatu 32, 87300, Lappajärvi",
+                                       "0700 123 123",
+                                       100000.00M,
+                                       78334.43M);
+
+            Company enterprise = new Company(firm);
+            enterprise.title = "akin muansiirtovirma";
+            enterprise.income = 300456.34M;
+
+
+            Console.WriteLine(firm.ToString());
+            Console.WriteLine(firm.Profit());
+            Console.WriteLine("__________________________________\n");
+            Console.WriteLine(enterprise.ToString());
+            Console.WriteLine(enterprise.Profit());
         }
     }
 }
