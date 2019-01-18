c9b0e00e91bb832443bdee092c435a36d8807770
akitiainen <aki.tiainen@hotmail.fi>
2019-01-18 11:20:13 +0200

    EmployeeTask
    initial commit

---
+++ OOHarjoitukset/EmployeeTask/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace EmployeeTask
+{
+    class Employee
+    {
+        public string name;
+        public int id;
+        public string position;
+        public string salary;
+        private static int counter = 1;
+
+        public Employee(string name, string position, string salary)
+        {
+            this.name = name;
+            this.id = counter++;
+            this.position = position;
+            this.salary = salary;
+        }
+
+        public string PrintEmployeeInfo()
+        {
+            return $"Työntekijä:\t {name}\nid:\t\t {id}\nAsema:\t\t {position}\n" +
+                $"Palkka:\t\t {salary}";
+        }
+
+        public void ComapreSalary(Employee x)
+        {
+
+        }
+    }
+}
---
+++ OOHarjoitukset/EmployeeTask/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EmployeeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[3];
+            employees[0] = new Employee("make",  "työm mies", "kalja kontti");
+            employees[1] = new Employee("pete",  "johtJA", "maken vaimo");
+            employees[2] = new Employee("pate",  "alinta paskaa", "5snt");
+            foreach (Employee item in employees)
+            {
+                Console.WriteLine($"{item.PrintEmployeeInfo()}\n" +
+                    $"_________________________________\n");
+            }
+            
+        }
+    }
+}
