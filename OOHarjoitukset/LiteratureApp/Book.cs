a7892c469f187b114ad5435408c788179ed46c5b
akitiainen <aki.tiainen@hotmail.fi>
2019-02-05 11:31:49 +0200

    LiteratureApp v0.1
    initial commit

---
+++ OOHarjoitukset/LiteratureApp/Book.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace LiteratureApp
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public string publisher;
+        public double price;
+        public static string theme;
+
+        public Book()
+        {
+            Console.WriteLine("Syötä kirjan nimi:");
+            title = Console.ReadLine();
+            Console.WriteLine("Syötä kirjailijan:");
+            author = Console.ReadLine();
+            Console.WriteLine("Syötä julkaisija:");
+            publisher = Console.ReadLine();
+            Console.WriteLine("Syötä kirjan hinta:");
+            while (!double.TryParse(Console.ReadLine(), out price))
+            {
+                Console.WriteLine("syötä hinta numeroina");
+            }
+            Console.WriteLine("Syötä kirjan teema:");
+            theme = Console.ReadLine();
+        }
+
+        public string GetBook(Book book)
+        {
+            string s = string.Empty;
+            if (book.title != title)
+            {
+                s = "väärä kirja lol";
+            }
+            else
+            {
+                s = $"title:\t{title}\nauthor:\t{author}\npublisher:\t{publisher}\n" +
+                    $"price:\t{price.ToString("c", CultureInfo.CurrentCulture)}\n" +
+                    $"theme:\t{theme}";
+            }
+            return s;
+        }
+
+        public static void ChangeTheme()
+        {
+
+        }
+    }
+}
---
+++ OOHarjoitukset/LiteratureApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LiteratureApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Book ass = new Book();
+            Console.Clear();
+            Console.WriteLine(ass.GetBook(ass));
+
+        }
+    }
+}
