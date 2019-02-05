98c5d98801d1dc6ea7dc5d56c3d426daeea2da9e
akitiainen <aki.tiainen@hotmail.fi>
2019-02-05 18:41:52 +0200

    LiteratureApp v0.2

--- OOHarjoitukset/LiteratureApp/Book.cs
+++ OOHarjoitukset/LiteratureApp/Book.cs
@@ -12,12 +12,20 @@ namespace LiteratureApp
         public string publisher;
         public double price;
         public static string theme;
-
+        
+        public Book(string title, string author, string publisher, double price)
+        {
+            this.title = title;
+            this.author = author;
+            this.publisher = publisher;
+            this.price = price;
+            
+        }
         public Book()
         {
             Console.WriteLine("Syötä kirjan nimi:");
             title = Console.ReadLine();
-            Console.WriteLine("Syötä kirjailijan:");
+            Console.WriteLine("Syötä kirjailija:");
             author = Console.ReadLine();
             Console.WriteLine("Syötä julkaisija:");
             publisher = Console.ReadLine();
@@ -39,16 +47,17 @@ namespace LiteratureApp
             }
             else
             {
-                s = $"title:\t{title}\nauthor:\t{author}\npublisher:\t{publisher}\n" +
-                    $"price:\t{price.ToString("c", CultureInfo.CurrentCulture)}\n" +
-                    $"theme:\t{theme}";
+                s = $"title:\t\t{title}\nauthor:\t\t{author}\npublisher:\t{publisher}\n" +
+                    $"price:\t\t{price.ToString("c", CultureInfo.CurrentCulture)}\n" +
+                    $"theme:\t\t{theme}\n____________________________________\n";
             }
             return s;
         }
 
         public static void ChangeTheme()
         {
-
+            Console.WriteLine("Annas uus teema");
+            theme = Console.ReadLine();
         }
     }
 }
--- OOHarjoitukset/LiteratureApp/Program.cs
+++ OOHarjoitukset/LiteratureApp/Program.cs
@@ -7,9 +7,17 @@ namespace LiteratureApp
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Book ass = new Book();
+            Book book = new Book("Harry Potter ja Viisasten Kivi", "J.K. Rowling", "Tammi", 24.95);
+            Book book2 = new Book();
             Console.Clear();
-            Console.WriteLine(ass.GetBook(ass));
+            Console.WriteLine(book.GetBook(book));
+            Console.WriteLine(book2.GetBook(book2));
+            Book.ChangeTheme();
+            Console.WriteLine("Paina näppäintä vaihtaaksesi teemaa");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine(book.GetBook(book));
+            Console.WriteLine(book2.GetBook(book2));
 
         }
     }
