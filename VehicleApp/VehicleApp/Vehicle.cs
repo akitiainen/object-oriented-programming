b3fc5b66c879a6ce60984af28b6d0e4595c4e478
akitiainen <aki.tiainen@hotmail.fi>
2019-02-19 14:42:53 +0200

    VehicleApp
    initial commit

---
+++ VehicleApp/VehicleApp/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace VehicleApp
+{
+    class Vehicle
+    {
+        protected string _type;
+        protected string _brand;
+        protected string _year;
+        protected double _price;
+
+        public Vehicle()
+        {
+            
+        }
+
+
+
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine($"brand:t\t{_brand}" +
+                $"manufacturing year:\t{_year}" +
+                $"price\t\t{_price.ToString("c", CultureInfo.CurrentCulture)}");
+        }
+    }
+
+
+}
