865e1a3e18b92b10b2651786efd130bd16e22a76
akitiainen <aki.tiainen@hotmail.fi>
2019-03-03 22:55:29 +0200

    InterfaceTask
    initial commit

---
+++ InterfaceTask/InterfaceTask/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceTask
+{
+    interface IProduct
+    {
+        void GetProduct();
+        void CalculateTotal();
+    }
+    class Product : IProduct
+    {
+        protected string name;
+        protected double price;
+        protected int count;
+
+        void IProduct.GetProduct()
+        {
+
+        }
+
+        void IProduct.CalculateTotal()
+        {
+
+        }
+    }
+}
---
+++ InterfaceTask/InterfaceTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfaceTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
