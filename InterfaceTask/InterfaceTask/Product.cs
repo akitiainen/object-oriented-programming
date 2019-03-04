8142aa402780cfa1dda25a11f20bcf2f4c1254ad
akitiainen <aki.tiainen@hotmail.fi>
2019-03-04 15:21:29 +0200

    InterfaceTask v0.1
    first step complete

--- InterfaceTask/InterfaceTask/Product.cs
+++ InterfaceTask/InterfaceTask/Product.cs
@@ -1,28 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Globalization;
 
 namespace InterfaceTask
 {
     interface IProduct
     {
-        void GetProduct();
-        void CalculateTotal();
+        Product GetProduct(string name);
+        double CalculateTotal();
     }
     class Product : IProduct
     {
-        protected string name;
-        protected double price;
-        protected int count;
+        public string name;
+        public double price;
+        public int amount;
 
-        void IProduct.GetProduct()
+        
+
+        public Product(string name, double price, int amount)
         {
-
+            this.name = name;
+            this.price = price;
+            this.amount = amount;
         }
 
-        void IProduct.CalculateTotal()
+        public override string ToString()
         {
+            return $"Name: {name}\tPrice: {price:F2} €\tAmount: {amount}";
+        }
 
+        Product IProduct.GetProduct(string name)
+        {
+            if (this.name == name)
+                return this;
+            else
+                return null;
+        }
+
+        double IProduct.CalculateTotal()
+        {
+            return price * Convert.ToDouble(amount);
         }
     }
 }
--- InterfaceTask/InterfaceTask/Program.cs
+++ InterfaceTask/InterfaceTask/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace InterfaceTask
 {
@@ -6,7 +8,23 @@ namespace InterfaceTask
     {
         static void Main(string[] args)
         {
-            
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            List<Product> productList = new List<Product>()
+            {
+                new Product("iids", 100500, 13),
+                new Product("ags", 13.25, 5),
+                new Product("doge", 100000000, 1)
+            };
+
+            productList.ForEach(Console.WriteLine);
+            foreach (IProduct item in productList)
+            {
+                Console.WriteLine(item.CalculateTotal().ToString("c", CultureInfo.CurrentCulture));
+            }
+            /*product.ToString();
+            Console.WriteLine(product.GetProduct("iids"));
+            Console.WriteLine(product.CalculateTotal().ToString("c", CultureInfo.CurrentCulture));*/
         }
     }
 }
