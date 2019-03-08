731b92ba64c2f9230ea45dee1d9dca94472587b8
akitiainen <aki.tiainen@hotmail.fi>
2019-03-08 23:54:34 +0200

    FileManagerTask v1.0
    task done

--- FileManagerTask/FileManagerTask/FileManager.cs
+++ FileManagerTask/FileManagerTask/FileManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using Newtonsoft.Json;
+using System.Globalization;
 
 namespace FileManagerTask
 {
@@ -15,39 +17,68 @@ namespace FileManagerTask
 
     class FileManager
     {
-        private string filePath;
+        private string _filePath;
+        private List<Food> _foodsList;
 
         public FileManager(string filePath)
         {
-            this.filePath = String.Empty;
+            _filePath = filePath;
         }
 
         public string ReadWords()
         {
-            string content = String.Empty;
-
-            content = ReadFile();
-
-            return content;
+            try
+            {
+                string content = ReadFile();
+                return content;
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+            
         }
 
         private string ReadFile()
         {
-            if (!File.Exists(filePath))
+            if (!File.Exists(_filePath))
             {
                 throw new FileNotFoundException("File not available");
             }
 
-            string directoryName = Path.GetDirectoryName(filePath);
-            string fileName = Path.GetFileName(filePath);
-            string fileExtension = Path.GetExtension(filePath);
+            string directoryName = Path.GetDirectoryName(_filePath);
+            string fileName = Path.GetFileName(_filePath);
+            string fileExtension = Path.GetExtension(_filePath);
 
             Console.WriteLine("directoryName: " + directoryName);
             Console.WriteLine("fileName: " + fileName);
             Console.WriteLine("fileExtension: " + fileExtension);
 
-            string fileContent = File.ReadAllText(filePath, Encoding.UTF8);
+            string fileContent = File.ReadAllText(_filePath, Encoding.UTF8);
             return fileContent;
+        }
+
+        public void SetJson()
+        {
+            List<Food> foods = new List<Food>()
+            {
+                new Food {Name = "kalja", Price = 12},
+                new Food {Name = "makkara", Price = 7},
+                new Food {Name = "muna", Price = 9}
+            };
+
+            File.WriteAllText(@"C:\dev\TEMP\test.json", JsonConvert.SerializeObject(foods));
+        }
+
+        public void GetJson()
+        {
+            StreamReader reader = new StreamReader(_filePath);
+            string json = reader.ReadToEnd();
+            _foodsList = JsonConvert.DeserializeObject<List<Food>>(json);
+            foreach (var item in _foodsList)
+            {
+                Console.WriteLine($"Food: {item.Name}\tPrice: {item.Price.ToString("c", CultureInfo.CurrentCulture)}");
+            }
         }
     }
 }
--- FileManagerTask/FileManagerTask/Program.cs
+++ FileManagerTask/FileManagerTask/Program.cs
@@ -6,7 +6,15 @@ namespace FileManagerTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            FileManager fileManager = new FileManager(@"C:\dev\TEMP\test.txt");
+            Console.WriteLine(fileManager.ReadWords());
+            Console.WriteLine("________________________\n");
+
+            FileManager fileManagerJson = new FileManager(@"C:\dev\TEMP\test.json");
+            fileManagerJson.SetJson();
+            fileManagerJson.GetJson();
         }
     }
 }
